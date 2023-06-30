45336626ac329d8303b9c7f59d11436143c626e1
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-06-30 14:11:45 +0500

    Add LoggingMiddleware

---
+++ WebAppMVC/Middlewares/LoggingMiddlewares.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WebAppLearnNet5.Middlewares
+{
+    public class LoggingMiddlewares
+    {
+        private readonly RequestDelegate _next;
+
+        public LoggingMiddlewares(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            LogConsole(context);
+            //await LogFile(context);
+            await _next.Invoke(context);
+        }
+
+        private async Task LogFile(HttpContext context)
+        {
+            string logFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Logs", "RequestLog.txt");
+            var logsList = new List<string> { $"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}" };
+            await File.AppendAllLinesAsync(logFilePath, logsList);
+        }
+        private void LogConsole(HttpContext context) => Console.WriteLine($"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}");
+    }
+}
