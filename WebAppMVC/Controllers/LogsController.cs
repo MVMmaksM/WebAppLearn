32a05ddc26304d5293b99b62424b094027c100ac
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-07-17 10:34:07 +0500

    Add LogsController

--- WebAppMVC/Controllers/LogsController.cs
+++ WebAppMVC/Controllers/LogsController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebAppMVC.DB.LoggingRepository;
 
 namespace WebAppMVC.Controllers
 {
     public class LogsController : Controller
     {
+        private ILoggingRepository _loggingRepository;
+
+        public LogsController(ILoggingRepository loggingRepository)
+        {
+            _loggingRepository = loggingRepository;
+        }
         public IActionResult Index()
         {
-            return View();
+            var requests = _loggingRepository.GetRequests();
+            return View(requests);
         }
     }
 }
